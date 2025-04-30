0bbb011e3f278f3e9768b93acb14c10a78d62392
nickd10 <nickdossin@gmail.com>
2025-04-30 13:14:32 -0400

    Update

--- assignment2.js
+++ assignment2.js
@@ -1,8 +1,26 @@
 async function getQuote() {
-    const response = await fetch('https://zenquotes.io/api/random');
-    const data = await response.json();
+    const quoteResponse = await fetch('https://zenquotes.io/api/random');
+    const data = await quoteResponse.json();
 
     document.getElementById("quote").innerHTML = `"${data[0].q}" - ${data[0].a}`;
 }
 
-window.onload = getQuote;
+async function getStockData() {
+    const tickerResponse = await fetch('https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2023-01-09/2023-02-10?adjusted=true&sort=asc&limit=120&apiKey=YpKlIbuK2gAapw4d8PdTceJuANip7n0Z');
+    const tickerData = await tickerResponse.json();
+
+    console.log('Retrieved Data: ', tickerData);
+}
+
+async function getDogImages() {
+    const dogImageResponse = await fetch('https://dog.ceo/api/breeds/image/random/10');
+    const dogImageData = await dogImageResponse.json();
+
+    console.log('Retrieved Data: ', dogImageData);
+}
+
+window.onload = async function() {
+    await getQuote();
+    await getStockData();
+    await getDogImages();
+};
